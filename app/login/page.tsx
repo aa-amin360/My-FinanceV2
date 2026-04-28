68c285d7c0556285cdf8ac2431f39493c323d7d1
Abdullah Al-Amin <101693098+aa-amin360@users.noreply.github.com>
2026-04-28 23:54:54 +0600

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <button
+        onClick={() => signIn("google")}
+        className="px-6 py-3 bg-green-500 rounded-xl"
+      >
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
