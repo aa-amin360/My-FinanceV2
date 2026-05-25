2c37cb1412930ab8235bf733dfafc0fe5aee3013
Abdullah Al-Amin <101693098+aa-amin360@users.noreply.github.com>
2026-05-26 01:22:44 +0600

    Comment out AmbientPlayer in layout.tsx
    Comment out AmbientPlayer component in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans`}>
         <ThemeProvider>
-          <AmbientPlayer />
+          {/* <AmbientPlayer /> */}
           {children}
         </ThemeProvider>
       </body>
